56717eeb2bdf1ba655dde2efb994af5e87db1b27
Chirapapan <tapsangwan@gmail.com>
2016-04-19 09:16:52 +0200

    ooo
    sjdhfjd

--- TheExposureGame/Assets/Scripts/SavePlayerInput.cs
+++ TheExposureGame/Assets/Scripts/SavePlayerInput.cs
@@ -4,5 +4,11 @@ using System.Collections;
 
 public class SavePlayerInput : MonoBehaviour {
 
+	string playerNewInput;
 
+	public void OnChange(string newInput)
+	{
+		playerNewInput = newInput;
+		Debug.Log (playerNewInput);
+	}
 }

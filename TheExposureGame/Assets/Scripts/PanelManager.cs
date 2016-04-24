69edf093d98e0a583525926144e75a0341c3d207
flyzza <zfy1995@hotmail.com>
2016-04-24 22:50:25 +0200

    Added clickable continents

--- TheExposureGame/Assets/Scripts/PanelManager.cs
+++ TheExposureGame/Assets/Scripts/PanelManager.cs
@@ -1,28 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PanelManager : MonoBehaviour {
+public class PanelManager : MonoBehaviour
+{
 
-	public Panel CurrentOption; 
-	private CanvasGroup canvasGroupCurrentOption;
+    public Panel CurrentOption;
+    private CanvasGroup canvasGroupCurrentOption;
 
-	private void Awake() {
-		//_canvasGroup = GetComponent<CanvasGroup> ();
-		canvasGroupCurrentOption = CurrentOption.GetComponent<CanvasGroup> ();
-	}
+    private void Awake()
+    {
+        //_canvasGroup = GetComponent<CanvasGroup> ();
+        canvasGroupCurrentOption = CurrentOption.GetComponent<CanvasGroup>();
+    }
 
-	private void Start() {
-		Option (CurrentOption);
-		//_canvasGroup.alpha = 1;
-	}
+    private void Start()
+    {
+        Option(CurrentOption);
+        //_canvasGroup.alpha = 1;
+    }
 
-	public void Option(Panel option) {
-		if (CurrentOption != null) {
-			CurrentOption.IsOpen = false;
-		}
+    public void Option(Panel option)
+    {
+        if (CurrentOption != null)
+        {
+            CurrentOption.IsOpen = false;
+        }
 
-		CurrentOption = option;
-		CurrentOption.IsOpen = true; 
-		canvasGroupCurrentOption.alpha = 1;
-	}
+        CurrentOption = option;
+        CurrentOption.IsOpen = true;
+        canvasGroupCurrentOption.alpha = 1;
+    }
 }
---
+++ TheExposureGame/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject panelManagerObj;
+    public Panel continentPanelObj;
+    public Image mapSprite;
+    public Image rewardSprite;
+    public Text continentText;
+
+    private PanelManager panelManagerScript;
+    
+
+    void Awake()
+    {
+        panelManagerScript = panelManagerObj.GetComponent<PanelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 8))
+            {
+                if (hit.collider.tag == "Continent")
+                {
+                    Continent hitContinent = hit.transform.gameObject.GetComponent<Continent>();
+                    mapSprite.sprite = hitContinent.continentSprite;
+                    rewardSprite.sprite = hitContinent.rewardSprite;
+                    continentText.text = hitContinent.continentName;
+                    panelManagerScript.Option(continentPanelObj);
+                }
+            }
+        }
+    }
+}
